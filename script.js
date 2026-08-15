fc37870e0d67ad24717e2c2bf0fe5246768551b9
rohit73848 <rohit.fiverr2006@gmail.com>
2026-08-15 12:33:52 +0530

    feat: implement continuous snake movement logic for all directions

--- script.js
+++ script.js
@@ -5,14 +5,45 @@ const blockHeight = 50;
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
+const direction = "down";
+
 const blocksArr = [];
+const snake = [
+  { x: 1, y: 3 },
+  // { x: 1, y: 4 },
+  // { x: 1, y: 5 },
+];
 for (row = 0; row < rows; row++) {
   for (col = 0; col < cols; col++) {
     const block = document.createElement("div");
     block.classList.add("block");
     board.append(block);
-    block.innerText = `${row} : ${col}`
+    block.innerText = `${row} : ${col}`;
     blocksArr[`${row} : ${col}`] = block;
   }
 }
-console.log(blocksArr)
+
+function render() {
+  snake.forEach((segment) => {
+    blocksArr[`${segment.x} : ${segment.y}`].classList.add("fill");
+  });
+}
+
+setInterval(() => {
+  let head = null;
+  if (direction === "left") {
+    head = { x: snake[0].x, y: snake[0].y - 1 };
+  } else if (direction === "right") {
+    head = { x: snake[0].x, y: snake[0].y + 1 };
+  } else if (direction === "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  } else if (direction === "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
+  }
+  snake.forEach((segment) => {
+    blocksArr[`${segment.x} : ${segment.y}`].classList.remove("fill");
+  });
+  snake.unshift(head);
+  snake.pop();
+  render();
+}, 500);
