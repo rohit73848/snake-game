09417eefbb04dec5dec469d111dc1345aa051305
rohit73848 <rohit.fiverr2006@gmail.com>
2026-08-15 11:44:17 +0530

    refactor: implement 2D coordinate system for grid blocks and update block size

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const board = document.querySelector(".board");
-const blockWidth = 30;
-const blockHeight = 30;
+const blockWidth = 50;
+const blockHeight = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
-for (let i = 0; i < rows * cols; i++) {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  board.append(block);
+const blocksArr = [];
+for (row = 0; row < rows; row++) {
+  for (col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.append(block);
+    block.innerText = `${row} : ${col}`
+    blocksArr[`${row} : ${col}`] = block;
+  }
 }
+console.log(blocksArr)
